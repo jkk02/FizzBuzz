cc77ae978ab74d8df13fa609187305416f73fde3
Kappicht.Jannik <Kappicht.Jannik@SK-D43D7EB3B863.2631.skool.net>
2018-09-20 09:21:10 +0200

    FizzBuzz

---
+++ src/FissBuss.java
@@ -0,0 +1,39 @@
+
+public class FissBuss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+ for(int i = 1; i < 1000000; i++)	{	
+	 System.out.println(i+": ");
+		
+	 if(divider3){
+		 System.out.println("Fizz");
+		 if(divider5){
+			 System.out.println("Buzz");
+		 }
+		 else{
+			 System.out.println("");
+		 }
+	 }
+	 
+	 
+	 public static boolean divider3(int i){
+		 if(i%3==0){
+			 return true;
+		 }
+		 else{return false;}
+	 }
+	 
+	 
+	 public static boolean divider5(int i){
+		 if(i%5==0){
+			 return true;
+		 }
+		 else{return false;}
+	 }
+	
+	
+	
+	
+	
